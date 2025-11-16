c9d50e09804cb0d21382b1c522ab378b85a64e2e
muhammad naufal <mmnaufal688@gmail.com>
2025-11-16 21:47:10 +0700

    Update main.cpp

--- modul 3/guided/main.cpp
+++ modul 3/guided/main.cpp
@@ -1,1 +1,11 @@
+// testing
+#include <iostream>
+#include "mahasiswa.h"
+using namespace std;
 
+int main() {
+    mahasiswa mhs;
+    inputMhs(mhs);
+    cout << "Rata-rata nilai = " << rata2(mhs) << endl;
+    return 0;
+}
