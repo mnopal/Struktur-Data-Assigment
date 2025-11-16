c1a066defcfb096e89e609a0bde4c3648baf4f3e
muhammad naufal <mmnaufal688@gmail.com>
2025-11-16 21:31:16 +0700

    Update mahasiswa.h

--- modul 3/guided/mahasiswa.h
+++ modul 3/guided/mahasiswa.h
@@ -1,1 +1,12 @@
+#ifndef MAHASISWA_H_INCLUDED
+#define MAHASISWA_H_INCLUDED
 
+struct  mahasiswa {
+    /* data */
+    char nim[10];
+    int nilai1, nilai2;
+};
+
+void inputMhs(mahasiswa &m);
+float rata2(mahasiswa m);
+#endif // MAHASISWA_H_INCLUDED
