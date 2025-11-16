5db3545af26a7a08a666032274118fc7d1610d87
muhammad naufal <mmnaufal688@gmail.com>
2025-11-16 21:29:59 +0700

    Update mahasiswa.cpp

--- modul 3/guided/mahasiswa.cpp
+++ modul 3/guided/mahasiswa.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+#include "mahasiswa.h"
+
+void inputMhs(mahasiswa &m) {
+    cout << "Input nim = ";
+    cin >> (m).nim;
+    cout << "Input nilai 1 = ";
+    cin >> (m).nilai1;
+    cout << "Input nilai 2 = ";
+    cin >> (m).nilai2;
+}
+
+float rata2(mahasiswa m) {
+    return (m.nilai1 + m.nilai2) / 2.0;
+}
+
 
