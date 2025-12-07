eb6ce19641da6b30ca905b3f04342a62325b49b6
muhammad naufal <mmnaufal688@gmail.com>
2025-12-07 08:59:07 +0700

    Add header file for pelajaran ADT and functions

--- modul 3/unguided/soal2_pelajaran.h
+++ modul 3/unguided/soal2_pelajaran.h
@@ -1,1 +1,18 @@
+#ifndef PELAJARAN_H
+#define PELAJARAN_H
+
+#include <string>
+using namespace std;
+
+// ADT pelajaran
+struct pelajaran {
+    string namaMapel;
+    string kodeMapel;
+};
+
+// Deklarasi fungsi
+pelajaran create_pelajaran(string namapel, string kodepel);
+void tampil_pelajaran(pelajaran pel);
+
+#endif
 
