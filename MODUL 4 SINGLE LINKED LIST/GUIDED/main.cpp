5daca97b5bf6c45f726b69da356e2ee4ea0c184b
muhammad naufal <mmnaufal688@gmail.com>
2025-12-07 10:10:53 +0700

    Add linked list operations in main.cpp
    Implement linked list operations including insertion and deletion of nodes.

--- MODUL 4 SINGLE LINKED LIST/GUIDED/main.cpp
+++ MODUL 4 SINGLE LINKED LIST/GUIDED/main.cpp
@@ -1,1 +1,49 @@
+#include "list.h"
 
+#include<iostream>
+using namespace std;
+
+int main(){
+    linkedlist List;
+    address nodeA, nodeB, nodeC, nodeD, nodeE = nil;
+    createList(List);
+
+    dataMahasiswa mhs;
+
+    nodeA = alokasi("Dhimas", "2311102151", 20);
+    nodeB = alokasi("Arvin", "2211110014", 21);
+    nodeC = alokasi("Rizal", "2311110029", 20);
+    nodeD = alokasi("Satrio", "2211102173", 21);
+    nodeE = alokasi("Joshua", "2311102133", 21);
+
+    insertFirst(List, nodeA);
+    insertLast(List, nodeB);
+    insertAfter(List, nodeC, nodeA);
+    insertAfter(List, nodeD, nodeC);
+    insertLast(List, nodeE);
+
+    cout << "--- ISI LIST SETELAH DILAKUKAN INSERT ---" << endl;
+    printList(List);
+    //lanjutan
+    cout << "Jumlah Node: " << nbList(List) << endl;
+    cout << endl;
+
+    delFirst(List);
+    delLast(List);
+    delAfter(List, nodeD, nodeC);
+
+    cout << "--- ISI LIST SETELAH DILAKUKAN DELETE ---" << endl;
+    printList(List);
+    cout << "Jumlah Node: " << nbList(List) << endl;
+    cout << endl;
+    
+    deleteList(List);
+    cout << "--- ISI LIST SETELAH DILAKUKAN DELETE ---" << endl;
+    printList(List);
+    cout << "Jumlah Node: " << nbList(List) << endl;
+    cout << endl;
+
+    return 0;
+}
+
+//note: jika ingin hapus list maka lepas dulu pointer previousnya agar bisa hapus node
